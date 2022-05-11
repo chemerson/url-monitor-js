3d465870d6c477e1870d9b6c4be996bf9737743f
Chris Emerson <cme210@yahoo.com>
2022-05-11 09:11:15 -0400

    added urls

--- url-monitor-config.js
+++ url-monitor-config.js
@@ -2,7 +2,10 @@
 const urlList = [
    // 'https://www.wikipedia.com/',
    // 'https://www.wikinews.org/',
-    'https://www.pnc.com/en/personal-banking.html'
+    'https://www.pnc.com/en/personal-banking.html',
+    'https://secure.pnc.com/web-auth/enrollment/verify-identity',
+    'https://www.pnc.com/en/corporate-and-institutional.html',
+    'https://www.pnc.com/en/customer-service.html?lnksrc=topnav'
 ]
 
 const breakPoints = [1080, 896, 640, 530]
