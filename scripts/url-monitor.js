9253fb284adadf458b06e2e65a13123ba32be108
Chris Emerson <cme210@yahoo.com>
2022-11-26 10:48:11 -0500

    Update url-monitor.js

--- scripts/url-monitor.js
+++ scripts/url-monitor.js
@@ -121,7 +121,7 @@ const perf = require("execution-time")();
 
       try {
         await eyes.open(driver, config.appName, urls[i - 1].toString());
-        await eyes.check(urls[i - 1].toString(), Target.window().fully());
+        await eyes.check(urls[i - 1].toString(), Target.window().fully().layout(By.css('#message-bar > div > div > div > div > div.cf-sw-inner-container.cf-mb-left-align > div.cf-sw-countdown-wrap'));
         await eyes.close(false);
       } catch (err) {
         console.log("eyes.check ERROR: " + err.message);
